407d76c063da6c03fbe8142699035a59f323a3eb
swims_hjkl <sumedhrao7@gmail.com>
2025-01-11 23:52:47 +0530

    validation as a member method

--- png/png_types.go
+++ png/png_types.go
@@ -11,6 +11,17 @@ type PNG struct {
 	fileName string
 }
 
+func (png PNG) validatePNG() error {
+	var headerBytes = []byte{0X89, 0X50, 0X4E, 0X47, 0X0D, 0X0A, 0X1A, 0x0A}
+	if png.bytesContent == nil {
+		return errors.New("png content cannot be nil")
+	}
+	if len(png.bytesContent) < 8 || !utils.BytesEqual(headerBytes, png.bytesContent[0:8]) {
+		return errors.New("png header is not valid")
+	}
+	return nil
+}
+
 func NewPNG(bytesContent []uint8, fileName string) (*PNG, error) {
 	if (len(bytesContent) == 0) || (bytesContent == nil) {
 		return nil, errors.New("bytesContent cannot be empty or nil")
@@ -18,19 +29,10 @@ func NewPNG(bytesContent []uint8, fileName string) (*PNG, error) {
 	if fileName == "" {
 		return nil, errors.New("fileName cannot be empty")
 	}
-	if validatePng(bytesContent) != nil{
-		return nil, validatePng(bytesContent)
+	png := &PNG{bytesContent, fileName} 
+	if png.validatePNG() != nil {
+		return nil, png.validatePNG()
 	}
-	return &PNG{bytesContent, fileName}, nil
+	return png, nil
 }
 
-func validatePng(pngContent []uint8) error {
-	var headerBytes = []byte{0X89, 0X50, 0X4E, 0X47, 0X0D, 0X0A, 0X1A, 0x0A}
-	if pngContent == nil {
-		return errors.New("png content cannot be nil")
-	}
-	if len(pngContent) < 8 || !utils.BytesEqual(headerBytes, pngContent[0:8]) {
-		return errors.New("png header is not valid")
-	}
-	return nil
-}
